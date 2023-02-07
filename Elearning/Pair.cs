fadd17ccd1999dff7c1953556906fd1fffe9e963
gigilya <118998483+gigilya@users.noreply.github.com>
2023-02-07 22:48:45 +0300

    Update Pair.cs

--- Elearning/Pair.cs
+++ Elearning/Pair.cs
@@ -1,10 +1,20 @@
 using Microsoft.VisualBasic;
 using System;
 
-public class Pair
+class Pair
 {
-    public Pair()
-    {
+    private TimeSpan beginningsP;
+    private TimeSpan endingP;
+    private TimeSpan beginningsBreak;
+    private TimeSpan endingBreak;
+    private Shift shift;
 
+    public Pair(TimeSpan beginningsP, TimeSpan endingP, TimeSpan beginningsBreak, TimeSpan endingBreak, Shift shift)
+    {
+        this.beginningsP = beginningsP;
+        this.endingP = endingP;
+        this.beginningsBreak = beginningsBreak;
+        this.endingBreak = endingBreak;
+        this.shift = shift;
     }
 }
