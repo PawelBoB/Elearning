fbea0995c3342ac4265ead17428cf896d0c41a88
SashaArt01 <118837360+SashaArt01@users.noreply.github.com>
2023-02-09 13:56:59 +0300

    Update and rename Elearning/Discipline.cs to Электронное обучение/Discipline.cs

--- Elearning/Discipline.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.VisualBasic;
-using System;
-
-public class Discipline
-{
-    public Discipline()
-    {
-    }
-}
---
+++ Электронное обучение/Discipline.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualBasic;
+using System;
+
+public class Discipline
+{
+    private string name;
+    private string abbreviation;
+    public Discipline(string name, string abbreviation)
+    {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+}
