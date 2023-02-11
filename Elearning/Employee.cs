84e468a3497413162b81ddd53b80d6035554a07b
Portum6 <123866696+Portum6@users.noreply.github.com>
2023-02-11 17:19:57 +0300

    Update Employee.cs

--- Elearning/Employee.cs
+++ Elearning/Employee.cs
@@ -1,9 +1,17 @@
 using Microsoft.VisualBasic;
 using System;
 
-public class Employee
-{
-    public Employee()
+public class Employee {
+    private string fam;
+    private string name;
+    private string otch;
+    private Post post
+ 
+    public Employee(string fam, string name, string otch, Post post)
     {
+        this.fam = fam;
+        this.name = name;
+        this.otch = otch;
+        this.post = post;
     }
 }
