3bbec14ff8af5df8fc623d3994f626c5ffc5aa44
Asar_kun <63390256+y9NBA@users.noreply.github.com>
2023-02-12 21:00:30 +0300

    Update Building.cs

--- Elearning/Building.cs
+++ Elearning/Building.cs
@@ -3,7 +3,15 @@ using System;
 
 public class Building
 {
-    public Building()
+    private string name;
+    private string address;
+    private Employee employee;
+    private Organization organization;
+    public Building(string name, string address, Employee employee, Organization organization)
     {
+        this.name = name;
+        this.address = address;
+        this.employee = employee;
+        this.organization =organization;
     }
 }
