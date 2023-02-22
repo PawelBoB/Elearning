22b9a4babfca0de474462065700c37c29d3b5f1d
PawelBoB <118815705+PawelBoB@users.noreply.github.com>
2023-02-22 10:07:03 +0300

    Update Post.cs

--- Elearning/Post.cs
+++ Elearning/Post.cs
@@ -3,21 +3,12 @@ using System;
 
 public class Post
 {
-<<<<<<< HEAD
     private string name;
     private int salary;
     private Division division;
     public Post(string name, int salary, Division division)
     {
         this.name = name;
-=======
-    private string postname;
-    private int salary ;
-    private Division division;
-    public Post(string postname, int salary, Division division)
-    {
-        this.postname = postname;
->>>>>>> 144be28 (2.0)
         this.salary = salary;
         this.division = division;
     }
