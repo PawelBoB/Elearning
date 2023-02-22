e40a30a91f4355ea3c868c69eea2583dcd759d01
ARTYW375 <118816744+ARTYW375@users.noreply.github.com>
2023-02-22 12:56:13 +0300

    Update Paragraph.cs

--- Elearning/Paragraph.cs
+++ Elearning/Paragraph.cs
@@ -1,11 +1,15 @@
 using Microsoft.VisualBasic;
 using System;
+//https://docs.google.com/document/d/1yVUsN3kjBq8p0lpNqIPmfKwpoLFw12PQTUwH_bwbODc/edit?usp=share_link
 
 public class Paragraph
 {
-    private string name;
-    private Theme theme;
-    
+    private readonly string name;
+    public string Name { get { return name; } }
+
+    private readonly Theme theme;
+    public Theme Theme { get { return theme; } }
+
     public Paragraph(string name, Theme theme)
     {
         this.name = name;
