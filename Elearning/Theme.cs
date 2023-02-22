7700cf99009896887a8778d4116c95e8f827b291
PawelBoB <118815705+PawelBoB@users.noreply.github.com>
2023-02-22 10:13:55 +0300

    Update Theme.cs

--- Elearning/Theme.cs
+++ Elearning/Theme.cs
@@ -3,15 +3,9 @@ using System;
 
 public class Theme
 {
-<<<<<<< HEAD
-    public Theme()
-    {
-=======
     private string name;
     public Theme(string name)
     {
         this.name = name;
-
->>>>>>> 144be28 (2.0)
     }
 }
