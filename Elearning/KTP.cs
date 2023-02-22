71431cbcba09a9de98de4f89f7151b75ae8c804a
Asar_kun <63390256+y9NBA@users.noreply.github.com>
2023-02-22 13:21:18 +0300

    Update KTP.cs

--- Elearning/KTP.cs
+++ Elearning/KTP.cs
@@ -5,5 +5,19 @@ public class KTP
 {
     public KTP()
     {
+        private Paragraph paragraph;
+        private LessonType lessontype;
+        private List<Content> content;
+        private int nomer;
+        private int chas;
+
+        public Student(Paragraph paragraph, LessonType lessontype, List<Content> content, int nomer, int chas)
+        {
+            this.paragraph = paragraph;
+            this.lessontype = lessontype;
+            this.content = content;
+            this.nomer = nomer;
+            this.chas = chas;
+        }
     }
 }
