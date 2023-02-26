70ceb74d7f3a4133073fefb6563e6472922e6449
Asar_kun <63390256+y9NBA@users.noreply.github.com>
2023-02-26 19:18:00 +0300

    fix class Student

--- Elearning/Student.cs
+++ Elearning/Student.cs
@@ -3,13 +3,19 @@ using System;
 
 namespace Elearning
 {
-    public class Student
+    class Student
     {
         private readonly string fam;
         private readonly string name;
         private readonly string otch;
         private readonly Group group;
         private readonly DateTime? birthday;
+
+        public string Fam { get => fam; }
+        public string Name { get => name; }
+        public string Otch { get => otch; }
+        public Group GroupI { get => group; }
+        public DateOnly BirthDay { get => DateOnly.FromDateTime(Convert.ToDateTime(birthday)); }
 
         public Student(string fam, string name, string otch, Group group, DateTime? birthday = null)
         {
