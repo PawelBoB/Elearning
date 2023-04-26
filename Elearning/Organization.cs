64f5abc09ddfabc655aa4c99e67d33bd277b201a
ArtemVavilov <118816967+ArtemVavilov@users.noreply.github.com>
2023-04-26 09:07:00 +0300

    Update Organization.cs
    New Organization

--- Elearning/Organization.cs
+++ Elearning/Organization.cs
@@ -4,24 +4,20 @@ using System;
 
 namespace Elearning
 {
+
     public class Organization
     {
-        private readonly string name;
-        private readonly object legalAddress;
-        private readonly object actualAddress;
-        private readonly Employee employee;
+        public string Name { get; }
+        public string LegalAddress { get; }
+        public string ActualAddress { get; }
+        public Employee Employee { get; }
 
-        public string Name { get { return name; } }
-        public object LegalAddress { get { return legalAddress; } }
-        public object ActualAddress { get { return actualAddress; } }
-        public Employee Employee { get { return employee; } }
-
-        public Organization(string name, object legalAddress, object actualAddress, Employee employee)
+        public Organization(string name, string legalAddress, string actualAddress, Employee employee)
         {
-            this.name = name;
-            this.legalAddress = legalAddress;
-            this.actualAddress = actualAddress;
-            this.employee = employee;
+            Name = name;
+            LegalAddress = legalAddress;
+            ActualAddress = actualAddress;
+            Employee = employee;
         }
     }
 }
