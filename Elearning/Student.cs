8da539f64ddd17cc85825edb5fd5379d2fe250a9
Asar_kun <63390256+y9NBA@users.noreply.github.com>
2023-05-11 09:23:58 +0300

    ЛР 06 Вариант 05 Student

--- Elearning/Student.cs
+++ Elearning/Student.cs
@@ -11,7 +11,10 @@ namespace Elearning
         public string MiddleName { get; }
         public Group Group { get; }
         public DateOnly Birthday { get; }
-
+        public Student() : this("Noname", "Noname", "Noname")
+        { }
+        public Student(string lastName, string name, string middleName) : this(lastName, name, middleName, new Group(), new DateOnly(0, 0, 0))
+        { }
         public Student(string lastName, string name, string middleName, Group group, DateOnly birthday)
         {
             LastName = lastName;
@@ -19,6 +22,42 @@ namespace Elearning
             MiddleName = middleName;
             Group = group;
             Birthday = birthday;
+        }
+        public void Deconstruct(out string studentlastName, out string studentname, out string studentmiddleName, out Group studentgroup, out DateOnly studentbirthday)
+        {
+            studentlastName = LastName;
+            studentname = Name;
+            studentmiddleName = MiddleName;
+            studentgroup = Group;
+            studentbirthday = Birthday;
+        }
+        static public void PrintName(string Name)
+        {
+            Console.WriteLine($"Привет, {Name}.");
+        }
+        static public void PrintName(string Name, string LasrName)
+        {
+            Console.WriteLine($"{LasrName} {Name}, привет.");
+        }
+        static public void PrintName(string Name, string LasrName, string MiddleName)
+        {
+            Console.WriteLine($"{LasrName} {Name} {MiddleName}, здравствуйте.");
+        }
+        readonly struct StudentStruct
+        {
+            public string LastName { get; }
+            public string Name { get; }
+            public string MiddleName { get; }
+            public Group Group { get; }
+            public DateOnly Birthday { get; }
+            public StudentStruct(Student student)
+            {
+                LastName = student.LastName;
+                Name = student.Name;
+                MiddleName = student.MiddleName;
+                Group = student.Group;
+                Birthday = student.Birthday;
+            }
         }
     }
 }
