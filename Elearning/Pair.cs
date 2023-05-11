4cd7b82a761ad8691bc3a785289e621f9182f144
gigilya <118998483+gigilya@users.noreply.github.com>
2023-05-11 11:15:16 +0300

    Pair.cs 6 laba

--- Elearning/Pair.cs
+++ Elearning/Pair.cs
@@ -9,14 +9,15 @@ namespace Elearning
 {
     public class Pair       // Pair num = new Pair(new TimeSpan (12,23,04)); - ввод времени
     {
-        public TimeSpan BeginningsP { get; }
-        public TimeSpan EndingP { get; }
-        public TimeSpan BeginningsBreak { get; }
-        public TimeSpan EndingBreak { get; }
-        public Shift Shift { get; }
-        
+        public TimeSpan BeginningsP { get; set; }
+        public TimeSpan EndingP { get; set; }
+        public TimeSpan BeginningsBreak { get; set; }
+        public TimeSpan EndingBreak { get; set; }
+        public Shift Shift { get; set; }
+
         public Pair() : this(new TimeSpan(), new TimeSpan(), new TimeSpan()) { }
-        public Pair(TimeSpan eginningsP, TimeSpan endingP, TimeSpan beginningsBreak) : this(eginningsP, endingP, beginningsBreak, new TimeSpan(), new Shift("")) { }
+        public Pair(TimeSpan eginningsP, TimeSpan endingP, TimeSpan beginningsBreak) : 
+                    this(eginningsP, endingP, beginningsBreak, new TimeSpan(), new Shift("")) { }
         public Pair(TimeSpan beginningsP, TimeSpan endingP, TimeSpan beginningsBreak, TimeSpan endingBreak, Shift shift)
         {
             BeginningsP = beginningsP;
@@ -25,13 +26,42 @@ namespace Elearning
             EndingBreak = endingBreak;
             Shift = shift;
         }
-        public void Deconstract(out TimeSpan beginningsP, out TimeSpan endingP, out TimeSpan beginningsBreak, out TimeSpan endingBreak, out Shift shift)
+        public void Deconstract(out TimeSpan beginningsP, out TimeSpan endingP, out TimeSpan beginningsBreak, 
+                                out TimeSpan endingBreak, out Shift shift)
         {
             beginningsP = BeginningsP;
             endingP = EndingP;
             beginningsBreak = BeginningsBreak;
             endingBreak = EndingBreak;
             shift = Shift;
+        }
+        struct Pair_
+        {
+            public TimeSpan BeginningsP { get; set; }
+            public TimeSpan EndingP { get; set; }
+            public TimeSpan BeginningsBreak { get; set; }
+            public TimeSpan EndingBreak { get; set; }
+            public Shift Shift { get; set; }
+            public Pair_(Pair pair)
+            {
+                BeginningsP = pair.BeginningsP;
+                EndingP = pair.EndingP;
+                BeginningsBreak = pair.BeginningsBreak;
+                EndingBreak = pair.EndingBreak;
+                Shift = pair.Shift;
+            }
+        }
+         static public void PrintPair(TimeSpan BeginningsP)
+        {
+            Console.WriteLine($"Пара начинается {BeginningsP}");
+        }
+        static public void PrintPair(TimeSpan BeginningsP, TimeSpan BeginningsBreak)
+        {
+            Console.WriteLine($"Пара начинается {BeginningsP}, пара закончивается {BeginningsBreak}");
+        }
+        static public void PrintPair(TimeSpan BeginningsP, TimeSpan EndingBreak, TimeSpan EndingP)
+        {
+            Console.WriteLine($"\"Пара начинается {BeginningsP},перерыв начинается {EndingBreak}, перерыв закончивается {EndingP}");
         }
     }
 }
