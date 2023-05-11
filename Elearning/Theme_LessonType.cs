d47108ae9185ddcc67fd74d846a0fb823f91ad80
user <user@412-05.kpc408.ru>
2023-05-11 11:36:59 +0300

    Вариант 19

--- Elearning/Theme_LessonType.cs
+++ Elearning/Theme_LessonType.cs
@@ -7,16 +7,46 @@ namespace Elearning
 {
     public class Theme_LessonType
     {
-        public Theme Theme { get; }
-        public LessonType LessonType { get; }
-        public UInt16 Hours { get; }
+        public struct Structure
+        {
+            public Theme theme;
+            public LessonType lessonType;
+            public UInt16 hours;
 
+            public Structure(Theme theme, LessonType lessonType, UInt16 hours)
+            {
+                this.theme = theme;
+                this.lessonType = lessonType;
+                this.hours = hours;
+            }
+        }
+
+        public Theme Theme { get; set; }
+        public LessonType LessonType { get; set; }
+        public UInt16 Hours { get; set; }
+        public Structure structure { get; set; }
 
         public Theme_LessonType(Theme theme, LessonType lessonType, UInt16 hours)
         {
             Theme = theme;
             LessonType = lessonType;
             Hours = hours;
+            structure = new Structure(theme, lessonType, hours);
         }
+
+        public Theme_LessonType(Theme theme, LessonType lessonType) : this(theme, lessonType, 0) { }
+
+        public Theme_LessonType() : this(new Theme(""), new LessonType(""), 0) { }
+
+        public void Deconstruct(out Theme theme, out LessonType lessonType, out UInt16 hours)
+        {
+            theme = Theme;
+            lessonType = LessonType;
+            hours = Hours;
+        }
+
+        public void foo() { }
+        public void foo(int value) { }
+        public void foo(string value) { }
     }
 }
