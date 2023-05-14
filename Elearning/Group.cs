81f96330bd9738b5bdfc152d9e6a7da1f3b2f4dc
Dmitrii Osipov <dimaosipov907@gmail.com>
2023-05-14 19:17:29 +0300

    Вариант 4

--- Elearning/Group.cs
+++ Elearning/Group.cs
@@ -1,30 +1,68 @@
 //https://docs.google.com/document/d/1qZEaADy71rJCOuNSTpyhh-f-NhSL2a6DCm88EgFO20o/edit?usp=sharing
 using Microsoft.VisualBasic;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Elearning
 {
     public class Group
     {
-        public string Name { get; }
-        public string ShortName { get; }
-        public byte Size { get; }
-        public UInt16 YearAdmission { get;  }
-        public Specialization Specialization { get; }
-        public Employee Headmaster { get; } 
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public byte Size { get; set; }
+        public UInt16 YearAdmission { get; set; }
+        public Specialization Specialization { get; set; }
+        public Employee Headmaster { get; set; } 
 
-        public Group(string name, string shortName, byte size, UInt16 yearAdmission, Specialization special, Employee headmaster)
+        public Group() : this("Noname", "Noname")
+        { }
+        public Group(string name, string shortName) : this(name, shortName, 25, 2021, new Specialization(), new Employee())
+        { }
+        public Group(string name, string shortName, byte size, UInt16 yearAdmission, Specialization specialization, Employee headmaster)
         {
             Name = name;
             ShortName = shortName;
             Size = size;
             YearAdmission = yearAdmission;
-            Specialization = special;
+            Specialization = specialization;
             Headmaster = headmaster;
         }
-        #pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
-        public Group() //Заглушка для конструктора класса Student
-#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
-        { }
+        public void Deconstruct(out string group_name, out string group_shortName, out byte group_size, out UInt16 group_yearAdmission, out Specialization group_specialization, out Employee group_headmaster )
+        {
+            group_name = Name;
+            group_shortName = ShortName;
+            group_size = Size;
+            group_yearAdmission = YearAdmission;
+            group_specialization = Specialization;
+            group_headmaster = Headmaster;
+        }
+        public void Print(string name)
+        {
+            Console.WriteLine($"Полное название: {name}");
+        }
+        public void Print(string name, string shortname)
+        {
+            Console.WriteLine($"Полное название: {name}, Сокращенное название: {shortname}");
+        }
+        public void Print(string name, string shortname, byte size)
+        {
+            Console.WriteLine($"Полное название: {name}, Сокращенное название: {shortname}, Численность: {size}");
+        }
+        struct Group_Struct
+        {
+            public string Name { get; set; }
+            public string ShortName { get; set; }
+            public byte Size { get; set; }
+            public Specialization Specialization { get; set; }
+            public Employee Headmaster { get; set; }
+            public Group_Struct(Group group)
+            {
+                Name = group.Name;
+                ShortName = group.ShortName;
+                Size = group.Size;
+                Specialization = group.Specialization;
+                Headmaster = group.Headmaster;
+            }
+        }
     }
 }
